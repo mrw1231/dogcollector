9feface2d62f49e347c2439bc08ed266bb894d37
Michael White <michael@Michaels-MacBook-Pro.local>
2022-01-13 15:12:37 -0700

    Model Set Up

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+SIZES = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large')
+)
+
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
-    description= models.TextField(max_length=250)
+    size = models.CharField(
+        max_length=1,
+        choices=SIZES,
+        default=SIZES[0][0]
+    )
     age = models.IntegerField()
+    description= models.TextField(max_length=250)
 
     def __str__(self):
         return self.name
