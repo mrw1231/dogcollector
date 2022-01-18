3a2cb9604be5a04e3687f9355c942e6cdbb76be2
Michael White <mrwhite1231@gmail.com>
2022-01-17 19:34:11 -0700

    Feeding Model Added

--- main_app/models.py
+++ main_app/models.py
@@ -7,6 +7,12 @@ SIZES = (
     ('L', 'Large')
 )
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
@@ -22,5 +28,17 @@ class Dog(models.Model):
     def __str__(self):
         return self.name
     
-    # def get_absolute_url(self):
-    #     return reverse('dogs_detail', kwargs={'dog_id': self.id})
+    def get_absolute_url(self):
+        return reverse('dogs_detail', kwargs={'dog_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0],
+    )
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Create your views here.
@@ -15,3 +16,15 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', { 'dog': dog })
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = '__all__'
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
